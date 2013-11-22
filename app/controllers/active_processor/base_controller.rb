448469dc483254f99cf74955d5f787735d28939f
dimcha <avedenij@yahoo.com>
2013-11-22 14:36:28 +0200

    payment gateways active but limited

--- app/controllers/active_processor/base_controller.rb
+++ app/controllers/active_processor/base_controller.rb
@@ -4,17 +4,8 @@ module ActiveProcessor
     layout "callc" #layout ActiveProcessor.configuration.layout
     before_filter :check_post_method_pg, :only => [:pay]
     before_filter :check_localization
-    before_filter :check_if_gateway_enabled, :only => [:index]
     before_filter :check_if_enabled, :only => [:index, :pay]
-                   #before_filter :verify_params, :only => [ :pay ]
 
-                   #  verify :method => :post, :only => [ :pay ],
-                   #    :redirect_to => { :controller => "/callc", :action => :main },
-                   #    :add_flash => { :notice => _('Dont_be_so_smart'),
-                   #    :params => { :dont_be_so_smart => true }
-                   #  }
-
-                   # GET /{gateway}
     def index
       @gateway = ::GatewayEngine.find(:first, {:engine => params[:engine], :gateway => params[:gateway], :for_user => current_user.id}).enabled_by(current_user.owner.id).query
 
@@ -54,10 +45,10 @@ module ActiveProcessor
               if custom_redirect and custom_redirect.to_i == 1
                 redirect_to Web_URL + "/" + custom_redirect_successful_payment.to_s
               else
-                redirect_to :controller => "/callc", :action => "main"
+                redirect_to :root
               end
             else
-              redirect_to :controller => "/callc", :action => "main"
+              redirect_to :root
             end
 
           }
@@ -97,28 +88,21 @@ module ActiveProcessor
 
     private
 
-    def check_if_gateway_enabled
-      if !current_user or Confline.get_value([params['engine'], params['gateway'], "enabled"].join("_"), current_user.owner_id).to_i == "0"
-        flash[:notice] = _("Inactive_Gateway")
-        redirect_to :controller => "/callc", :action => "main"
-      end
-    end
-
     def check_user
-      redirect_to :controller => "/callc", :action => "main" if !session[:usertype_id] or session[:usertype] == "guest" or session[:usertype].to_s == ""
+      redirect_to :root if !session[:usertype_id] or session[:usertype] == "guest" or session[:usertype].to_s == ""
     end
 
     def verify_params
       unless params['gateways']
         dont_be_so_smart
-        redirect_to :controller => "/callc", :action => "main"
+        redirect_to :root
       end
     end
 
     def check_post_method_pg
       unless request.post?
         flash[:notice] = _('Dont_be_so_smart')
-        redirect_to :controller => "callc", :action => "main" and return false
+        redirect_to :root and return false
       end
     end
 
@@ -131,7 +115,7 @@ module ActiveProcessor
         end 
       else 
         flash[:notice] = _('Dont_be_so_smart') 
-        redirect_to :controller => "/callc", :action => "main" 
+        redirect_to :root
       end
     end 
 
